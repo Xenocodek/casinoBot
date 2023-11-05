dd42201b8be363e04c20df627f9725893c3db6d9
Xenocode <maxikote2012@gmail.com>
2023-11-05 20:46:20 +0300

    Refactoring Currency

--- handlers/currency_handler.py
+++ handlers/currency_handler.py
@@ -1,5 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
+from aiogram.exceptions import TelegramBadRequest
 
 from lexicon.subloader import JSONFileManager
 from utils.currency import CurrencyConverter
@@ -22,9 +23,19 @@ async def show_currency(callback: CallbackQuery):
 
     # Send an acknowledgement message
     await callback.answer(messages_data['accepted_req'])
-    
-    # Prepare the currency message
-    answer_message = await prepare_curency()
+    await callback.message.edit_text(messages_data['awaiting_req'])
 
-    # Edit the message with the currency message and the back main menu keyboard
-    await callback.message.edit_text(answer_message, reply_markup=back_main_menu_keyboard)
+    try:
+        answer_message = await prepare_curency()
+
+        await callback.message.delete()
+
+        # Send Message with the currency message and the back main menu keyboard
+        await callback.message.answer(answer_message)
+        await callback.message.answer(messages_data['select_command'], reply_markup=back_main_menu_keyboard)
+
+    except TelegramBadRequest:
+        await callback.message.edit_text(messages_data['req_error'], reply_markup=back_main_menu_keyboard)
+
+    except Exception:
+        await callback.message.edit_text(messages_data['req_error'], reply_markup=back_main_menu_keyboard)
