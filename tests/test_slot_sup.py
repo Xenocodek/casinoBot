8d1b0fbd8409214a61206c3dba3596a6d6af8a47
Xenocode <maxikote2012@gmail.com>
2023-11-07 17:58:34 +0300

    Added tests

--- tests/test_slot_sup.py
+++ tests/test_slot_sup.py
@@ -7,3 +7,14 @@ class TestSlotSup:
         assert format_number(3.14) == "3.1"
         assert format_number(2.0) == "2"
         assert format_number(5) == "5"
+
+    def test_negative_input(self):
+        assert format_number(-3.14) == "-3.1"
+        assert format_number(-2.0) == "-2"
+        assert format_number(-5) == "-5"
+
+    def test_none_input(self):
+        assert format_number(None) == None
+
+    def test_zero_input(self):
+        assert format_number(0) == "0"
