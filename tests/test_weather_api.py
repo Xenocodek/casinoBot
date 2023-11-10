5fd28525d7a08993c84251178fdb5eb9069575d6
Xenocode <maxikote2012@gmail.com>
2023-11-11 00:17:24 +0300

    Fix Tests Weather API

--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -1,15 +1,11 @@
 import logging
-import pytest
 import requests
-from datetime import datetime, timedelta
-import time
-from unittest.mock import patch, Mock
-from requests.exceptions import HTTPError, RequestException
 from utils.user_data import prepare_weather
 from utils.weather_get_api import GetWeather
 
 class TestGetWeather:
     def test_initialize_instance(self):
+        # Test case for initializing the GetWeather class instance
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
         assert weather.token == "12345"
         assert weather.city_id == "524311"
@@ -19,14 +15,15 @@ class TestGetWeather:
         assert weather.last_update is None
 
     def test_retrieve_weather_data(self, mocker):
-            weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
-            mock_response = mocker.Mock()
-            mock_response.json.return_value = {"temperature": 25, "humidity": 80}
-            mocker.patch('requests.get', return_value=mock_response)
+        # Test case for successfully retrieving weather data
+        weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
+        mock_response = mocker.Mock()
+        mock_response.json.return_value = {"temperature": 25, "humidity": 80}
+        mocker.patch('requests.get', return_value=mock_response)
 
-            data = weather.get_data_weather()
+        data = weather.get_data_weather()
 
-            assert data == {"temperature": 25, "humidity": 80}
+        assert data == {"temperature": 25, "humidity": 80}
 
     def test_return_cached_data(self, mocker):
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
@@ -40,6 +37,7 @@ class TestGetWeather:
         assert data1 == data2
 
     def test_send_get_request(self, mocker):
+        # Test case for returning cached weather data
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
         mock_response = mocker.Mock()
         mock_response.json.return_value = {"temperature": 25, "humidity": 70}
@@ -50,6 +48,7 @@ class TestGetWeather:
         assert data == {"temperature": 25, "humidity": 70}
 
     def test_api_request_404_error(self, mocker):
+        # Test case for sending a new GET request and retrieving weather data
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
         mocker.patch('requests.get', side_effect=requests.exceptions.HTTPError(response=mocker.Mock(status_code=404)))
 
@@ -58,6 +57,7 @@ class TestGetWeather:
         assert data is None
 
     def test_api_request_500_error(self, mocker):
+        # Test case for handling HTTP 500 error during API request
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
         mocker.patch('requests.get', side_effect=requests.exceptions.HTTPError(response=mocker.Mock(status_code=500)))
 
@@ -66,6 +66,7 @@ class TestGetWeather:
         assert data is None
 
     def test_log_http_error(self, mocker):
+        # Test case for logging HTTP error during API request
         weather = GetWeather(weather_token="12345", city_id="524311", unit='metric', lang='ru')
         mocker.patch('requests.get', side_effect=requests.exceptions.HTTPError("404 Not Found"))
         mocker.patch('logging.info')
@@ -75,6 +76,7 @@ class TestGetWeather:
         logging.info.assert_called_with("HTTP error occurred: 404 Not Found")\
         
     def test_log_other_error(self, mocker):
+        # Test case for logging other errors during API request
         weather = GetWeather()
         mocker.patch('requests.get', side_effect=requests.exceptions.RequestException("Other Error"))
         mocker.patch('logging.info')
@@ -84,6 +86,7 @@ class TestGetWeather:
         logging.info.assert_called_with("An error occurred: Other Error")
 
     def test_api_request_timeout(self, mocker):
+        # Test case for handling API request timeout
         weather = GetWeather()
         mocker.patch('requests.get', side_effect=requests.exceptions.Timeout)
 
@@ -92,6 +95,7 @@ class TestGetWeather:
         assert data is None
 
     def test_handle_invalid_weather_token(self, mocker):
+        # Test case for handling an invalid weather token during API request
         weather = GetWeather(weather_token="invalid_token", city_id="524311", unit='metric', lang='ru')
         mocker.patch('requests.get', side_effect=requests.exceptions.HTTPError("401 Unauthorized"))
         mock_logging = mocker.patch('logging.info')
@@ -100,80 +104,3 @@ class TestGetWeather:
 
         assert data is None
         mock_logging.assert_called_with("HTTP error occurred: 401 Unauthorized")
-
-
-class TestPrepareWeather:
-    def test_formatted_string_with_weather_info(self, mocker):
-        # Mock the weather.get_data_weather() method to return a sample weather data
-        mocker.patch.object(GetWeather, 'get_data_weather', return_value={
-            "name": "Moscow",
-            "weather": [{"description": "cloudy"}],
-            "main": {"temp": 20},
-            "wind": {"speed": 10}
-        })
-
-        # Invoke the prepare_weather function
-        result = prepare_weather()
-
-        # Assert that the result is a formatted string with the weather information
-        assert isinstance(result, str)
-        assert "Moscow" in result
-        assert "Cloudy" in result
-        assert "20°C" in result
-        assert "10 м/с" in result
-
-        # Returns a message indicating that weather data is not available when weather data is not available.
-    def test_message_when_weather_data_not_available(self, mocker):
-        # Mock the weather.get_data_weather() method to return None
-        mocker.patch.object(GetWeather, 'get_data_weather', return_value=None)
-
-        # Invoke the prepare_weather function
-        result = prepare_weather()
-
-        # Assert that the result is a message indicating that weather data is not available
-        assert isinstance(result, str)
-        assert result == "Weather data is not available at the moment."
-
-    def test_message_when_weather_data_is_empty_dict(self, mocker):
-        # Mock the weather.get_data_weather() method to return an empty dictionary
-        mocker.patch.object(GetWeather, 'get_data_weather', return_value={})
-
-        # Invoke the prepare_weather function
-        result = prepare_weather()
-
-        # Assert that the result is a message indicating that weather data is not available
-        assert isinstance(result, str)
-        assert result == "Weather data is not available at the moment."
-
-    def test_formatted_string_with_missing_values(self, mocker):
-        # Mock the weather.get_data_weather() method to return a sample weather data with missing values
-        mocker.patch.object(GetWeather, 'get_data_weather', return_value={
-            "name": "London",
-            "weather": [{"description": "cloudy"}],
-            "main": {},
-            "wind": {}
-        })
-
-        # Invoke the prepare_weather function
-        result = prepare_weather()
-
-        # Assert that the result is a formatted string with the weather information
-        assert isinstance(result, str)
-        assert "London" in result
-        assert "Cloudy" in result
-
-    def test_data_cache_and_update_time(self, mocker):
-        # Mock the weather.get_data_weather() method to return a sample weather data
-        mocker.patch.object(GetWeather, 'get_data_weather', return_value={
-            "name": "Moscow",
-            "weather": [{"description": "cloudy"}],
-            "main": {"temp": 20},
-            "wind": {"speed": 10}
-        })
-
-        # Invoke the get_data_weather method twice in a row
-        result1 = GetWeather().get_data_weather()
-        result2 = GetWeather().get_data_weather()
-
-        # Assert that the same data is returned and no new API call is made
-        assert result1 == result2
